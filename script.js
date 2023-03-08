6fcc11eb02ce237a02ebb8dc83e2edc415854f9f
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-03-09 02:01:33 +0300

    sorting js code

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // Selectors
 const alertBox = document.getElementById("alertBox");
 const alertClose = document.getElementById("alertClose");
-const refreshButton = document.querySelector('main button.refresh-button')
-const loginButton = document.querySelector('main button.log-in-button')
+const refreshButton = document.querySelector("main button.refresh-button");
+const loginButton = document.querySelector("main button.log-in-button");
 
 
 
@@ -13,7 +13,7 @@ const loginButton = document.querySelector('main button.log-in-button')
 
 
 // Variables
-let isAlertVisible = false
+let isAlertVisible = false;
 
 
 
@@ -24,7 +24,7 @@ let isAlertVisible = false
 // Functions
 alert = (msg) => {
   instructorCustomAlert(msg);
-}
+};
 
 
 
@@ -53,9 +53,9 @@ function instructorCustomAlert(msg) {
   closeAlert.id = "alertClose";
   alertBoxLabel.appendChild(closeAlert);
   closeAlert.onclick = () => {
-    alertBox.remove()
-    isAlertVisible = false
-  }
+    alertBox.remove();
+    isAlertVisible = false;
+  };
 
   // Create content's alert box
   const alertBoxContent = document.createElement("div");
@@ -68,83 +68,82 @@ function instructorCustomAlert(msg) {
 
   // Create input container
   const inputContainer = document.createElement("div");
-  inputContainer.classList.add('input')
+  inputContainer.classList.add("input");
   form.appendChild(inputContainer);
 
   // Create password input
   const passInput = document.createElement("input");
-  passInput.type = 'password'
-  passInput.placeholder = 'Password'
+  passInput.type = "password";
+  passInput.placeholder = "Password";
   inputContainer.appendChild(passInput);
 
   // Create password's label
   const labelInput = document.createElement("label");
-  labelInput.innerHTML = 'ادخل كلمة المرور'
+  labelInput.innerHTML = "ادخل كلمة المرور";
   inputContainer.appendChild(labelInput);
 
   // Create Show&Hide password icon
   const showPassIcon = document.createElement("i");
-  showPassIcon.className = 'fa-regular fa-eye-slash show-pass'
+  showPassIcon.className = "fa-regular fa-eye-slash show-pass";
   inputContainer.appendChild(showPassIcon);
   const hidePassIcon = document.createElement("i");
-  hidePassIcon.className = 'fa-regular fa-eye hide-pass'
+  hidePassIcon.className = "fa-regular fa-eye hide-pass";
   inputContainer.appendChild(hidePassIcon);
 
   // Create options container
   const optionsContainer = document.createElement("div");
-  optionsContainer.classList.add('options')
+  optionsContainer.classList.add("options");
   form.appendChild(optionsContainer);
 
   // Create submit button (OK)
   const okButton = document.createElement("input");
-  okButton.classList.add('ok-button')
-  okButton.type = 'submit'
-  okButton.value = "OK"
+  okButton.classList.add("ok-button");
+  okButton.type = "submit";
+  okButton.value = "OK";
   optionsContainer.appendChild(okButton);
 
   // Create cancel button (Cancel)
   const cancelButton = document.createElement("button");
-  cancelButton.classList.add('cancel-button')
-  cancelButton.type = 'button'
-  cancelButton.innerHTML = 'Cancel'
+  cancelButton.classList.add("cancel-button");
+  cancelButton.type = "button";
+  cancelButton.innerHTML = "Cancel";
   optionsContainer.appendChild(cancelButton);
 
 
 
   function handlePassInput() {
-    let lengthInputText = passInput.value.length
-    if (lengthInputText > '30')
-      passInput.value = passInput.value.slice(0, lengthInputText - 1)
+    let lengthInputText = passInput.value.length;
+    if (lengthInputText > "30")
+      passInput.value = passInput.value.slice(0, lengthInputText - 1);
   }
-  passInput.addEventListener('input', () => handlePassInput())
-
-
+  passInput.addEventListener("input", () => handlePassInput());
 
   showPassIcon.onclick = () => {
-    showPassIcon.style.display = 'none'
-    hidePassIcon.style.display = 'block'
-    passInput.type = 'text'
-  }
+    showPassIcon.style.display = "none";
+    hidePassIcon.style.display = "block";
+    passInput.type = "text";
+  };
 
   hidePassIcon.onclick = () => {
-    hidePassIcon.style.display = 'none'
-    showPassIcon.style.display = 'block'
-    passInput.type = 'password'
-  }
+    hidePassIcon.style.display = "none";
+    showPassIcon.style.display = "block";
+    passInput.type = "password";
+  };
 
 
 
   function handleSubmit(e) {
-    e.preventDefault()
-    if (passInput.value.length <= 6 || passInput.value.length > 30) 
-      passInput.style.borderBottomColor = '#ff4f4f'
+    e.preventDefault();
+    if (passInput.value.length <= 6 || passInput.value.length > 30)
+      passInput.style.borderBottomColor = "#ff4f4f";
     else {
-      passInput.style.borderBottomColor = '#55ff55'
+      passInput.style.borderBottomColor = "#55ff55";
       setTimeout(() => form.submit(), 1500);
     }
   }
-  form.addEventListener('submit', (e) => handleSubmit(e))
+  form.addEventListener("submit", (e) => handleSubmit(e));
 }
+
 
 
 
@@ -152,12 +151,11 @@ function instructorCustomAlert(msg) {
 
 
 // Events
-refreshButton.onclick = () => location.reload()
+refreshButton.onclick = () => location.reload();
 
 loginButton.onclick = () => {
   if (!isAlertVisible) {
-    alert()
-    isAlertVisible = true
+    alert();
+    isAlertVisible = true;
   }
-}
-
+};
